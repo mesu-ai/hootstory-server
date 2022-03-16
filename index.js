921fb2312d828cb983b30fd13769286e2d8901e8
mesu-ai <mesu.s.ss@gmail.com>
2022-03-16 11:13:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express= require('express');
+const app=express();
+const cors=require('cors');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
+const port=process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient } = require('mongodb');
+const { query } = require('express');
+
+const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.4yf1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+
+
+
+async function run() {
+    try {
+      
+      await client.connect();
+       const database = client.db("hootstory");
+       const storiesCollection = database.collection("stories");
+
+      console.log('connect to db');
+
+      //get stories
+
+        app.get('/hootstory',async(req,res)=>{
+          const cursor=storiesCollection.find({});
+          const result=await cursor.toArray();
+          res.send(result);
+
+        });
+
+
+      console.log("Connected successfully to server");
+
+
+    } finally {
+     
+    //  await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello hootstory');
+});
+
+app.listen(port,()=>{
+    console.log(`connect to port ${port}`);
+})
